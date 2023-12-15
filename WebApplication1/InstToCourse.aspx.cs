ffe231149f6ef7b97b51b71f09303ecfd0d1ebb9
samalouty <128055267+samalouty@users.noreply.github.com>
2023-12-15 14:01:28 +0200

    added foreign key checks for instructor and course and slot in InstToCourse page

--- WebApplication1/InstToCourse.aspx.cs
+++ WebApplication1/InstToCourse.aspx.cs
@@ -20,26 +20,64 @@ namespace WebApplication1
         {
             if (ValidateForm())
             {
-                String connStr = WebConfigurationManager.ConnectionStrings["Advising_System"].ToString();
-                SqlConnection conn = new SqlConnection(connStr);
+                try
+                {
+                    String connStr = WebConfigurationManager.ConnectionStrings["Advising_System"].ToString();
+                    SqlConnection conn = new SqlConnection(connStr);
 
-                int courseid = Int16.Parse(courseId.Text);
-                int instID = Int16.Parse(instructorId.Text);
-                int slotid = Int16.Parse(slotId.Text);
-
-
-                SqlCommand instLink = new SqlCommand("Procedures_AdminLinkInstructor", conn);
-                instLink.CommandType = CommandType.StoredProcedure;
-                instLink.Parameters.Add(new SqlParameter("@cours_id", courseid));
-                instLink.Parameters.Add(new SqlParameter("@instructor_id", instID));
-                instLink.Parameters.Add(new SqlParameter("@slot_id", slotid));
-                
+                    int courseid = int.Parse(courseId.Text);
+                    int instID = int.Parse(instructorId.Text);
+                    int slotid = int.Parse(slotId.Text);
 
 
-                conn.Open();
-                instLink.ExecuteNonQuery();
-                conn.Close();
-                Response.Write("Linked Instructor and Course to the Slot");
+                    SqlCommand instLink = new SqlCommand("Procedures_AdminLinkInstructor", conn);
+                    instLink.CommandType = CommandType.StoredProcedure;
+                    instLink.Parameters.Add(new SqlParameter("@cours_id", courseid));
+                    instLink.Parameters.Add(new SqlParameter("@instructor_id", instID));
+                    instLink.Parameters.Add(new SqlParameter("@slot_id", slotid));
+
+
+
+                    conn.Open();
+                    int x =  instLink.ExecuteNonQuery();
+
+                    if (x <= 0)
+                    {
+                     ClientScript.RegisterStartupScript(GetType(), "alert", "alert('Non-existing Slot. Please enter a valid Slot ID.');", true);
+                        conn.Close();
+                        return; 
+                    }
+                    conn.Close();
+                    Response.Write("Linked Instructor and Course to the Slot");
+                }
+                catch(SqlException sq) {
+                    String connStr = WebConfigurationManager.ConnectionStrings["Advising_System"].ToString();
+                    SqlConnection conn = new SqlConnection(connStr);
+
+                    int courseNum = int.Parse(courseId.Text);
+                    int instID = int.Parse(instructorId.Text);
+                    int slotid = int.Parse(slotId.Text);
+
+
+                    if (!IsIdValid(courseNum, "Course", "course_id", conn))
+                    {
+                        ClientScript.RegisterStartupScript(GetType(), "alert", "alert('Non-existing Course ID. Please enter a valid Course ID.');", true);
+                        return;
+                    }
+
+                    if (!IsIdValid(instID, "Instructor", "instructor_id", conn))
+                    {
+                        ClientScript.RegisterStartupScript(GetType(), "alert", "alert('Non-existing Instructor ID. Please enter a valid Instructor ID.');", true);
+                        return;
+                    }
+
+                    if (!IsIdValid(instID, "Instructor", "instructor_id", conn))
+                    {
+                        ClientScript.RegisterStartupScript(GetType(), "alert", "alert('Non-existing Instructor ID. Please enter a valid Instructor ID.');", true);
+                        return;
+                    }
+
+                }
             }
         }
 
@@ -73,6 +111,18 @@ namespace WebApplication1
 
 
             return true;
+        }
+
+        private bool IsIdValid(int id, string tableName, string columnName, SqlConnection connection)
+        {
+            SqlCommand command = new SqlCommand($"SELECT COUNT(*) FROM {tableName} WHERE {columnName} = @ID", connection);
+            command.Parameters.Add(new SqlParameter("@ID", id));
+
+            connection.Open();
+            int count = (int)command.ExecuteScalar();
+            connection.Close();
+
+            return count > 0;
         }
     }
 }
